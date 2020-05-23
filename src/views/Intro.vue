b4c636af32c707f365028b97551db23237dc3570
longfei Zhou <i0.0ia@qq.com>
2020-05-23 22:38:21 +0800

    sadf

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,38 +1,82 @@
 <template>
-  <div class="intro bg-gray-100">
+  <div class="intro ">
     <div
-      class="intro-item bg-red-200 sheng"
+      class="intro-item sheng"
       @mouseover="shengShow"
       @mouseleave="shengLeave"
       ref="sheng"
     >
-      <div class="intro-card sheng"></div>
+      <div class="intro-card sheng">
+        <!-- <p class="title">生</p> -->
+        <img src="../assets/home/sheng.svg" class=" object-center" />
+      </div>
+      <div class="content" id="sheng-content">
+        <p>
+          生，扮演除净、丑所扮角色以外的所有男性人物。包括专扮中年以上男性的“老生”、专扮年轻男性的“小生”、专扮擅长武艺的男性“武生”和扮演儿童的“娃娃生”。
+        </p>
+      </div>
+      <p class="title">
+        生
+      </p>
     </div>
     <div
-      class="intro-item bg-green-200 dan"
+      class="intro-item dan"
       @mouseover="danShow"
       @mouseleave="shengLeave"
       ref="dan"
     >
-      <div class="intro-card "></div>
+      <div class="intro-card dan">
+        <img src="../assets/home/dan.svg" class=" object-center" />
+      </div>
+      <div class="content" id="dan-content">
+        <p>
+          生，扮演除净、丑所扮角色以外的所有男性人物。包括专扮中年以上男性的“老生”、专扮年轻男性的“小生”、专扮擅长武艺的男性“武生”和扮演儿童的“娃娃生”。
+        </p>
+      </div>
+      <p class="title">
+        旦
+      </p>
     </div>
     <div
-      class="intro-item bg-blue-200 jing"
+      class="intro-item jing"
       @mouseover="jingShow"
       @mouseleave="shengLeave"
       ref="jing"
     >
-      <div class="intro-card "></div>
+      <div class="intro-card jing">
+        <img src="../assets/home/jing.svg" class=" object-center" />
+      </div>
+      <div class="content" id="jing-content">
+        <p>
+          生，扮演除净、丑所扮角色以外的所有男性人物。包括专扮中年以上男性的“老生”、专扮年轻男性的“小生”、专扮擅长武艺的男性“武生”和扮演儿童的“娃娃生”。
+        </p>
+      </div>
+      <p class="title">
+        净
+      </p>
     </div>
     <div
-      class="intro-item bg-teal-200 chou"
+      class="intro-item chou"
       @mouseover="chouShow"
       @mouseleave="shengLeave"
       ref="chou"
     >
-      <div class="intro-card "></div>
+      <div class="intro-card chou">
+        <img src="../assets/home/chou.svg" class=" object-center" />
+      </div>
+      <div class="content" id="chou-content">
+        <p>
+          生，扮演除净、丑所扮角色以外的所有男性人物。包括专扮中年以上男性的“老生”、专扮年轻男性的“小生”、专扮擅长武艺的男性“武生”和扮演儿童的“娃娃生”。
+        </p>
+      </div>
+      <p class="title">
+        丑
+      </p>
     </div>
-    <button class="backbutton shadow-md hover:shadow-xl transition-all duration-200" @click="$router.push('/')">
+    <button
+      class="backbutton shadow-md hover:shadow-xl transition-all duration-200"
+      @click="$router.push('/')"
+    >
       <div class="bg"></div>
     </button>
   </div>
@@ -40,44 +84,64 @@
 <script>
 export default {
   methods: {
+    elementclean(){
+            document.getElementById("sheng-content").style.opacity = "0";
+      document.getElementById("dan-content").style.opacity = "0";
+      document.getElementById("jing-content").style.opacity = "0";
+      document.getElementById("chou-content").style.opacity = "0";
+    },
     shengShow() {
+      this.elementclean()
       this.$refs.dan.style.transform = "translateX(25vw)";
       this.$refs.jing.style.transform = "translateX(25vw)";
       this.$refs.chou.style.transform = "translateX(25vw)";
+      document.getElementById("sheng-content").style.opacity = "1";
     },
     shengLeave() {
       this.$refs.dan.style.transform = "translateX(0)";
       this.$refs.jing.style.transform = "translateX(0)";
       this.$refs.chou.style.transform = "translateX(0)";
+      document.getElementById("sheng-content").style.opacity = "0";
+      // document.querySelector("sheng + div.content").style.opacity = "none";
     },
     danShow() {
       // this.$refs.sheng.style.transform = "translateX(25vw)";
+      this.elementclean()
       this.$refs.jing.style.transform = "translateX(25vw)";
       this.$refs.chou.style.transform = "translateX(25vw)";
+      document.getElementById("dan-content").style.opacity = "1";
     },
     danLeave() {
-      // this.$refs.sheng.style.transform = "translateX(0)";
+      // thi ；lk
+      this.$refs.sheng.style.transform = "translateX(0)";
       this.$refs.jing.style.transform = "translateX(0)";
       this.$refs.chou.style.transform = "translateX(0)";
+      document.getElementById("dan-content").style.opacity = "0";
+      console.log("danleave");
     },
     jingShow() {
-      // this.$refs.sheng.style.transform = "translateX(-25vw)";
+      this.elementclean()
+      this.$refs.jing.style.transform = "translateX(-25vw)";
       this.$refs.dan.style.transform = "translateX(-25vw)";
+      document.getElementById("jing-content").style.opacity = "1";
     },
     jingLeave() {
-      // this.$refs.sheng.style.transform = "translateX(25vw)";
       this.$refs.jing.style.transform = "translateX(0vw)";
       this.$refs.dan.style.transform = "translateX(0)";
+      document.getElementById("jing-content").style.opacity = "0";
     },
     chouShow() {
-      // this.$refs.sheng.style.transform = "translateX(-25vw)";
+      this.elementclean()
+      this.$refs.chou.style.transform = "translateX(-25vw)";
       this.$refs.dan.style.transform = "translateX(-25vw)";
       this.$refs.jing.style.transform = "translateX(-25vw)";
+      document.getElementById("chou-content").style.opacity = "1";
     },
     chouLeave() {
       // this.$refs.sheng.style.transform = "translateX(25vw)";
       this.$refs.dan.style.transform = "translateX(0)";
       this.$refs.jing.style.transform = "translateX(0)";
+      document.getElementById("chou-content").style.opacity = "0";
     },
   },
 };
@@ -106,9 +170,53 @@ export default {
     width: 25vw;
     display: inline-block;
     transition: all 0.5s ease-out;
+    .sheng {
+      background-color: #ffc2af;
+    }
+    .dan {
+      background-color: #a34136;
+    }
+    .jing {
+      background-color: #d3af8b;
+    }
+    .chou {
+      background-color: #3e476e;
+    }
   }
   .intro-card {
     height: 100%;
+    width: 50vw;
+    // opacity: 0.5;
+    display: flex;
+    img {
+      height: 50vh;
+      margin: auto;
+    }
+  }
+  p.title {
+    position: relative;
+    top: -90vh;
+    left: 5rem;
+    font-size: 2.5rem;
+    color: white;
+  }
+  div.content {
+    width: 50vw;
+    height: 100%;
+    background-color: rgba(22, 22, 22, 0.37);
+    position: absolute;
+    display: flex;
+    opacity: 0;
+    transition: all 0.5s ease-out;
+    top: 0;
+    p {
+      height: 40%;
+      width: 60%;
+      margin: auto;
+      font-size: 2rem;
+      color: white;
+      line-height: 4rem;
+    }
   }
 }
 </style>
--- src/views/Make.vue
+++ src/views/Make.vue
@@ -1,9 +1,49 @@
 <template>
   <div class="make flex w-full h-full">
-    <div class="left flex-1 bg-gray-300">
-      
+    <div class="left flex-1 flex flex-col">
+      <div class="title">
+        <p>京具</p>
+        <p>
+          Mask of <br />
+          Beijing Opera
+        </p>
+      </div>
+      <div class="flex-1">
+        <div class="canvas flex">
+          <div class="bar flex">
+            <div class="sheng flex-1">
+              <p>生</p>
+            </div>
+            <div class="dan flex-1">
+              <p>旦</p>
+            </div>
+            <div class="jing flex-1">
+              <p>净</p>
+            </div>
+            <div class="chou flex-1">
+              <p>丑</p>
+            </div>
+          </div>
+          <div class="main-canvas"></div>
+        </div>
+        <div class="w-full">
+          <button
+            class="completeButton py-3 px-10 text-white font-bold text-xl rounded-md items-end hover:shadow-lg transition-all duration-300 ease-in-out scale-105"
+          >
+            完成制作
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="right flex-1 bg-green-300"></div>
+
+    <div class="right flex-1">
+      <button
+        class="backbutton shadow-md hover:shadow-xl transition-all duration-200"
+        @click="$router.push('/')"
+      >
+        <div class="bg"></div>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -11,4 +51,93 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.left {
+  background-color: #3e476e;
+
+  .title {
+    color: #ffffff;
+    height: 20%;
+    padding: 3rem 4rem;
+    & p:first-child {
+      font-size: 2.5rem;
+      font-weight: bold;
+    }
+    & p:last-child {
+      padding-top: 2rem;
+      line-height: 2rem;
+    }
+  }
+  .canvas {
+    width: 100%;
+    height: 80%;
+    .bar {
+      height: 22rem;
+      width: 6rem;
+      margin-left: 4rem;
+      margin-top: 4rem;
+      flex-direction: column;
+      & > div {
+        margin: 0.4rem 0;
+        cursor: pointer;
+        user-select: none;
+        p {
+          font-size: 1.5rem;
+          color: white;
+          text-align: center;
+          padding: 1.2rem 0;
+        }
+        border: 1.5px solid rgba(209, 209, 209, 0.692);
+        border-radius: 7%;
+        &:hover {
+          background-color: rgba(209, 209, 209, 0.692);
+          &::before {
+            content: "";
+            display: block;
+            position: fixed;
+            background-color: #ff8e88;
+            height: 2.6rem;
+            width: 5px;
+            transform: translateY(1rem);
+          }
+        }
+      }
+      & div:first-child {
+        margin-top: 0;
+      }
+      & div:last-child {
+        margin-bottom: 0;
+      }
+    }
+    .main-canvas {
+      height: 100%;
+      width: 100%;
+    }
+  }
+  .completeButton {
+    background-color: #ff5c55;
+    float: right;
+    margin: 0 30%;
+  }
+}
+
+.right {
+  background-color: #ffffff;
+  .backbutton {
+  position: absolute;
+  bottom: 4rem;
+  right: 4rem;
+  width: 5rem;
+  height: 5rem;
+  background-color: #fff;
+  border-radius: 50%;
+  .bg {
+    background-image: url("../assets/back.svg");
+    background-size: cover;
+    margin: 1.5rem;
+    width: 2rem;
+    height: 2rem;
+  }
+}
+}
+</style>
